1d1e98c4feb5bdd270912196811eba9678038b55
Michael Adeyeye <commodorehansolo@gmail.com>
2026-01-13 04:39:49 +0100

    Included decimals

--- main.go
+++ main.go
@@ -6,17 +6,18 @@ import (
 
 func main(){
    
-   var t int
+   var t float64
    multiplier := []int{1,2,3,4,5,6,7,8,9,10,11,12}
    fmt.Print("Enter a number to see its times table: ")
-  
-  _, err := fmt.Scan(&t)  
+   
+   
+   _, err := fmt.Scan(&t)  
    if err != nil {
 	   fmt.Println("Invalid input:" , err)
 	   return
    }
    for _, i := range(multiplier){
-	   result := t * i
-	   fmt.Printf("%d x %d = %d\n", t, i, result)
+	   result := float64(t) * float64(i)
+	   fmt.Printf("%g x %d = %g\n", t, i, result)
    }
 }
